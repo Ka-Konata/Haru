9c44103c589d50dd9f06d44484845dc438debae8
Ka <69207529+Ka-Konata@users.noreply.github.com>
2022-12-02 20:10:29 -0300

    clear

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask("Haru v2")
+app = Flask("Haru v3")
 
 @app.route('/')
 def main():
-    return "The bot is alive!"
+    return "Mantendo a Haru funcionando :)"
 
 def run():
   app.run(host="0.0.0.0", port=8080)
